4ff572bd23eeb453237bb746879d15c278d1d74c
Maika Kanaka <maika-kanaka@protonmail.com>
2023-02-26 17:46:21 +0700

    100% width for video

--- page-templates/front.php
+++ page-templates/front.php
@@ -99,7 +99,7 @@ get_header(); ?>
                         </div>
                 <div class="col_right_eq col_2">
                         <div class="feat_thumb">
-                        <iframe height="315" src="https://www.youtube.com/embed/CGzRdwi7Yd0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/CGzRdwi7Yd0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         </div>
                 </div>
             </div>
